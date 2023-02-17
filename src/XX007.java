6702ebaa8161d9c8ebe3263f35d247f20d9aaac8
olliebhag <olliebhag@gmail.com>
2023-02-17 19:55:01 +0000

    idek

---
+++ src/XX007.java
@@ -0,0 +1,26 @@
+public class XX007 {
+    class student {
+        private String ID;
+        private String firstName;
+        private String surname;
+        private int interimTest;
+
+        private int[] homework = new int [5]; // check if zero indexed but prob fine
+
+        private double coursework;
+
+        private int exam;
+
+        private double weightedAverage;
+
+
+
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+}
